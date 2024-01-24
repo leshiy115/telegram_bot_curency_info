063e95ac12461918ea9965b9127946844fc782cc
Eminence_Grise <eminence_grise@inbox.ru>
2024-01-24 18:04:42 +0300

    all saved

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+TOKEN = '6716237363:AAFBTXLQuUg-9LQMDZBkCvgLqeZeXKj1GPg'
+
+keys = {
+    'евро': 'EUR',
+    'юань': 'CNY',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
+
+server = 'https://www.cbr-xml-daily.ru/daily_json.js'  # сайт ЦБ России
